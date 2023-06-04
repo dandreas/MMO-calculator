4982b23345f613324c05333fbf3e228362ffc925
Dillon Andreas <belfieldcecil@gmail.com>
2023-06-03 21:55:00 -0500

    Some initial input parsing work

--- mmo-calc.py
+++ mmo-calc.py
@@ -1,3 +1,22 @@
 import db
 
+cont = True
 
+while cont:
+    userInput = input(">")
+    inputList = userInput.split()
+
+    match inputList[0]:
+        case "help":
+            print("Test")
+        case "init":
+            print("Initializing database...")
+            db.initDb()
+            print("Database initialized!")
+        case "add":
+            print("Add")
+        case "calc":
+            print("cqlc")
+        case "exit":
+            print("Exiting...")
+            cont = False
